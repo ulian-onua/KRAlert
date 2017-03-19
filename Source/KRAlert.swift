1bfee44b010a646807c73fc5482f0714185de252
ulian_onua <ulian_onua@MacBook-Pro-Julian.local>
2017-03-19 19:32:56 +0200

    Some fixes

--- Source/KRAlert.swift
+++ Source/KRAlert.swift
@@ -12,37 +12,37 @@ import UIKit
 
 public class KRAlert {
     
-    static var okButtonTitle = "OK"
-    static var cancelButtonTitle = "Cancel"
-    static var yesButtonTitle = "Yes"
-    static var noButtonTitle = "No"
-    static var alertErrorTitle = "ERROR"
-    static var pickPhotoTitle  = "Pick Photo"
-    static var pickPhotoMessage  = "Choose source"
-    static var pickPhotoGalleryButtonTitle  = "From gallery"
-    static var pickPhotoCameraButtonTitle  = "From camera"
+    public static var okButtonTitle = "OK"
+    public static var cancelButtonTitle = "Cancel"
+    public static var yesButtonTitle = "Yes"
+    public static var noButtonTitle = "No"
+    public static var alertErrorTitle = "ERROR"
+    public static var pickPhotoTitle  = "Pick Photo"
+    public static var pickPhotoMessage  = "Choose source"
+    public static var pickPhotoGalleryButtonTitle  = "From gallery"
+    public static var pickPhotoCameraButtonTitle  = "From camera"
 
     
     //MARK: Alerts
 
-    class func alertOK(from viewController : UIViewController, title : String?, message : String?, completion:(()->Void)?) -> Void {
+    public class func alertOK(from viewController : UIViewController, title : String?, message : String?, completion:(()->Void)?) -> Void {
         alert(from: viewController, title: title, message: message, firstButtonTitle: okButtonTitle, firstButtonCompletion: completion, secondButtonTitle: nil, secondButtonCompletion: nil)
     }
     
-    class func alertOKCancel(from viewController : UIViewController, title : String?, message : String?, completion:(()->Void)?) -> Void {
+    public class func alertOKCancel(from viewController : UIViewController, title : String?, message : String?, completion:(()->Void)?) -> Void {
         alert(from: viewController, title: title, message: message, firstButtonTitle: cancelButtonTitle, firstButtonCompletion: nil, secondButtonTitle: okButtonTitle, secondButtonCompletion: completion)
     }
     
-    class func alertYesNo(from viewController : UIViewController, title : String?, message : String?, yesCompletion : (()->Void)?, noCompletion:(()->Void)?) -> Void {
+    public class func alertYesNo(from viewController : UIViewController, title : String?, message : String?, yesCompletion : (()->Void)?, noCompletion:(()->Void)?) -> Void {
         alert(from: viewController, title: title, message: message, firstButtonTitle: noButtonTitle, firstButtonCompletion: noCompletion, secondButtonTitle: yesButtonTitle, secondButtonCompletion: yesCompletion)
     }
     
     
-    class func alertError(from viewController : UIViewController, message : String?, completion:(()->Void)?) -> Void {
+    public class func alertError(from viewController : UIViewController, message : String?, completion:(()->Void)?) -> Void {
         alertOK(from: viewController, title: alertErrorTitle, message: message, completion: completion)
     }
     
-    class func alert(from viewController : UIViewController, title : String?, message : String?, firstButtonTitle : String?, firstButtonCompletion: (() -> Void)?, secondButtonTitle : String?, secondButtonCompletion: (() -> Void)?) -> Void {
+    public class func alert(from viewController : UIViewController, title : String?, message : String?, firstButtonTitle : String?, firstButtonCompletion: (() -> Void)?, secondButtonTitle : String?, secondButtonCompletion: (() -> Void)?) -> Void {
         var actions : [UIAlertAction] = []
         actions.append(UIAlertAction.init(title: firstButtonTitle, style: .cancel, handler: { (action) in
             if let firstButtonCompletion = firstButtonCompletion {
@@ -60,14 +60,14 @@ public class KRAlert {
         self.alert(from: viewController, title: title, message: message, actions: actions)
     }
     
-    class func alert(from viewController : UIViewController, title : String?, message : String?, actions : [UIAlertAction]?) {
+    public class func alert(from viewController : UIViewController, title : String?, message : String?, actions : [UIAlertAction]?) {
         abstractAlert(from: viewController, title: title, message: message, style: .alert, actions: actions)
     }
     
     //MARK: AlertWithTextField
     
     
-    class func alertOKCancel(from viewController : UIViewController, title : String?, message : String?, textFieldPlaceholder : String?, textFieldText : String?, secureEntry : Bool, completion: @escaping (String?)->Void,  cancelCompletion: (()->Void)?) ->Void {
+    public class func alertOKCancel(from viewController : UIViewController, title : String?, message : String?, textFieldPlaceholder : String?, textFieldText : String?, secureEntry : Bool, completion: @escaping (String?)->Void,  cancelCompletion: (()->Void)?) ->Void {
         
         DispatchQueue.main.async {
             let alertController = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
@@ -100,11 +100,11 @@ public class KRAlert {
     
     //MARK: Action Sheet
     
-    class func actionSheetPickPhoto(from viewController : UIViewController, galleryCompletion: @escaping ()->Void, cameraCompletion: @escaping ()->Void) -> Void {
+    public class func actionSheetPickPhoto(from viewController : UIViewController, galleryCompletion: @escaping ()->Void, cameraCompletion: @escaping ()->Void) -> Void {
         actionSheet(from: viewController, title: pickPhotoTitle, message: pickPhotoMessage, firstButtonTitle: pickPhotoGalleryButtonTitle, firstButtonCompletion: galleryCompletion, secondButtonTitle: pickPhotoCameraButtonTitle, secondButtonCompletion: cameraCompletion)
     }
     
-    class func actionSheet(from viewController : UIViewController, title : String?, message : String?, firstButtonTitle : String?, firstButtonCompletion: (() -> Void)?, secondButtonTitle : String?, secondButtonCompletion: (() -> Void)?) -> Void {
+    public class func actionSheet(from viewController : UIViewController, title : String?, message : String?, firstButtonTitle : String?, firstButtonCompletion: (() -> Void)?, secondButtonTitle : String?, secondButtonCompletion: (() -> Void)?) -> Void {
         var actions : [UIAlertAction] = []
         
         actions.append(UIAlertAction.init(title: firstButtonTitle, style: .default, handler: { (action) in
@@ -127,20 +127,20 @@ public class KRAlert {
         
     }
     
-    class func actionSheet(from viewController : UIViewController, title: String?, message : String?, actions: [UIAlertAction]?) -> Void {
+    public class func actionSheet(from viewController : UIViewController, title: String?, message : String?, actions: [UIAlertAction]?) -> Void {
         abstractAlert(from: viewController, title: title, message: message, style: .actionSheet, actions: actions)
     }
     
     //MARK: Abstract
 
-    class func abstractAlert(from viewController : UIViewController, title: String?, message : String?, style : UIAlertControllerStyle, actions: [UIAlertAction]?) -> Void {
+    public class func abstractAlert(from viewController : UIViewController, title: String?, message : String?, style : UIAlertControllerStyle, actions: [UIAlertAction]?) -> Void {
         DispatchQueue.main.async {
             let alertController = self.alertController(title: title, message: message, style: style, actions: actions)
             viewController.present(alertController, animated: true, completion: nil)
         }
     }
     
-    class func alertController(title: String?, message : String?, style : UIAlertControllerStyle, actions: [UIAlertAction]?) -> UIAlertController {
+    public class func alertController(title: String?, message : String?, style : UIAlertControllerStyle, actions: [UIAlertAction]?) -> UIAlertController {
         let alertController = UIAlertController.init(title: title, message: message, preferredStyle: style)
         
         // add actions to alertController
